1b07eb61b15a4b763213a142badb7cafe6dc1979
Karan Sharma <education.karu@example.com>
2026-02-02 01:58:31 +0530

    Just for prod Heroku

--- todo-app/web.py
+++ todo-app/web.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import functions
 
-todos = functions.get_todos('./todo-app/todos.txt')
+todos = functions.get_todos('todos.txt')
 
 def add_todo():
     todo = st.session_state['new_todo'] + '\n'
     todos.append(todo)
-    functions.write_todos(todos, "./todo-app/todos.txt")
+    functions.write_todos(todos, "todos.txt")
 
 # BTS = Behind the scenes.
 
@@ -19,7 +19,7 @@ for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key = todo)
     if checkbox:
         todos.pop(index)
-        functions.write_todos(todos,"./todo-app/todos.txt")
+        functions.write_todos(todos,"todos.txt")
         del st.session_state[todo] # In Python, 'del' is used to remove a property from a dictionary.
         st.rerun() # This will programmatically rerun the script. When we tick/untick a checkbox, that event will run the script and remove it from the list of 'todos' and from session state dictionary. Now, the extra 'st.rerun()' is to render the unchecked checkboxes.
 
